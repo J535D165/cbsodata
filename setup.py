d2b51115342a308ab788cc76f5bf311e0716fac8
Jonathan <jonathandebruinhome@gmail.com>
2016-12-18 23:42:55 +0100

    Fix installation issue and add license

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -17,13 +17,19 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+# "import" __version__ (trick out of https://github.com/spatialaudio/nbsphinx/blob/master/setup.py)
+for line in open('cbsodata.py'):
+    if line.startswith('__version__'):
+        exec(line)
+        break
+
 setup(
     name='cbsodata',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version=__version__,
 
     description='Statistics Netherlands opendata API client for Python',
     long_description=long_description,
@@ -68,6 +74,8 @@ setup(
     # packages=['cbsodata'],
 
     install_requires=['requests'],
+
+    py_modules=['cbsodata'],
 
     tests_require=[
         'nose',
