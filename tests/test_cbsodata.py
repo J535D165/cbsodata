07e30c530b755c561621c72ade4e658a9167e0b6
Jonathan de Bruin <jonathandebruinos@gmail.com>
2018-12-01 23:19:44 +0100

    Reconfigure tox and travis

--- tests/test_cbsodata.py
+++ tests/test_cbsodata.py
@@ -1,12 +1,13 @@
-import unittest
-from nose_parameterized import parameterized
-
 import os
 import shutil
 
 import requests
 
 import cbsodata
+
+# testing deps
+import pytest
+
 
 datasets = [
     '82010NED',
@@ -21,207 +22,223 @@ datasets_derden = [
 TEST_ENV = 'test_env'
 
 
-class TestCBSOData(unittest.TestCase):
+def setup_module(module):
+    print('\nsetup_module()')
 
-    @classmethod
-    def setupClass(self):
+    if not os.path.exists(TEST_ENV):
+        os.makedirs(TEST_ENV)
 
-        if not os.path.exists(TEST_ENV):
-            os.makedirs(TEST_ENV)
 
-    @classmethod
-    def tearDownClass(self):
-        shutil.rmtree(TEST_ENV)
+def teardown_module(module):
+    print('teardown_module()')
 
-    @parameterized.expand(datasets)
-    def test_info(self, table_id):
+    shutil.rmtree(TEST_ENV)
 
-        # testing
-        info = cbsodata.get_info(table_id)
+# Tests
 
-        self.assertIsInstance(info, dict)
 
-    @parameterized.expand(datasets)
-    def test_download(self, table_id):
+@pytest.mark.parametrize("table_id", datasets)
+def test_info(table_id):
 
-        cbsodata.download_data(table_id)
+    # testing
+    info = cbsodata.get_info(table_id)
 
-    @parameterized.expand(['00000AAA'])
-    def test_http_error(self, table_id):
+    assert isinstance(info, dict)
 
-        try:
-            cbsodata.get_data(table_id)
-        except requests.HTTPError as err:
-            assert True
-        else:
-            assert False
 
-    def test_http_error_table_list(self):
+@pytest.mark.parametrize("table_id", datasets)
+def test_download(table_id):
 
-        try:
-            cbsodata.get_table_list(catalog_url='test.cbs.nl')
-        except requests.ConnectionError as err:
-            assert True
-        else:
-            assert False
+    cbsodata.download_data(table_id)
 
-    @parameterized.expand(datasets)
-    def test_http_https_download(self, table_id):
 
-        cbsodata.options['use_https'] = True
-        cbsodata.download_data(table_id)
-        cbsodata.options['use_https'] = False
-        cbsodata.download_data(table_id)
-        cbsodata.options['use_https'] = True
+@pytest.mark.parametrize("table_id", ['00000AAA'])
+def test_http_error(table_id):
 
-    @parameterized.expand(datasets)
-    def test_download_and_store(self, table_id):
-
-        cbsodata.download_data(
-            table_id,
-            dir=os.path.join(TEST_ENV, table_id)
-        )
-
-        self.assertTrue(
-            os.path.exists(
-                os.path.join(TEST_ENV, table_id, 'TableInfos.json')
-            )
-        )
-
-    @parameterized.expand(datasets)
-    def test_get_data(self, table_id):
-
+    try:
         cbsodata.get_data(table_id)
+    except requests.HTTPError:
+        assert True
+    else:
+        assert False
 
-    @parameterized.expand(datasets)
-    def test_info_values(self, table_id):
 
-        info = cbsodata.get_info(table_id)
+def test_http_error_table_list():
 
-        # Check response is dict (not a list)
-        self.assertIsInstance(info, dict)
+    try:
+        cbsodata.get_table_list(catalog_url='test.cbs.nl')
+    except requests.ConnectionError:
+        assert True
+    else:
+        assert False
 
-        # Check required keys are available
-        self.assertTrue('Description' in info.keys())
-        self.assertTrue('ID' in info.keys())
-        self.assertTrue('Identifier' in info.keys())
 
-    def test_table_list(self):
+@pytest.mark.parametrize("table_id", datasets)
+def test_http_https_download(table_id):
 
-        self.assertGreaterEqual(len(cbsodata.get_table_list()), 100)
+    cbsodata.options['use_https'] = True
+    cbsodata.download_data(table_id)
+    cbsodata.options['use_https'] = False
+    cbsodata.download_data(table_id)
+    cbsodata.options['use_https'] = True
 
-    def test_filters(self):
 
-        default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
-        filters_and_selections = default_sel_filt.split("&")
+@pytest.mark.parametrize("table_id", datasets)
+def test_download_and_store(table_id):
 
-        for fs in filters_and_selections:
-            if fs.startswith('$filter='):
-                filt = fs[8:]
+    cbsodata.download_data(
+        table_id,
+        dir=os.path.join(TEST_ENV, table_id)
+    )
 
-        cbsodata.get_data('82070ENG', filters=filt)
+    assert os.path.exists(
+        os.path.join(TEST_ENV, table_id, 'TableInfos.json')
+    )
 
-    def test_select(self):
 
-        default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
-        filters_and_selections = default_sel_filt.split("&")
+@pytest.mark.parametrize("table_id", datasets)
+def test_get_data(table_id):
 
-        for fs in filters_and_selections:
-            if fs.startswith('$select='):
-                select = fs[8:]
+    cbsodata.get_data(table_id)
 
-        cbsodata.get_data('82070ENG', select=select)
 
-    def test_select_list(self):
+@pytest.mark.parametrize("table_id", datasets)
+def test_info_values(table_id):
 
-        default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
-        filters_and_selections = default_sel_filt.split("&")
+    info = cbsodata.get_info(table_id)
 
-        for fs in filters_and_selections:
-            if fs.startswith('$select='):
-                select = fs[8:]
+    # Check response is dict (not a list)
+    assert isinstance(info, dict)
 
-        cbsodata.get_data('82070ENG', select=select.split(', '))
+    # Check required keys are available
+    assert 'Description' in info.keys()
+    assert 'ID' in info.keys()
+    assert 'Identifier' in info.keys()
 
-    def test_select_subset(self):
 
-        default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
-        filters_and_selections = default_sel_filt.split("&")
+def test_table_list():
 
-        for fs in filters_and_selections:
-            if fs.startswith('$select='):
-                select = fs[8:]
+    assert len(cbsodata.get_table_list()) > 100
 
-        select_list = select.split(', ')
-        cbsodata.get_data('82070ENG', select=select_list[0:2])
 
-    def test_select_n_cols(self):
+def test_filters():
 
-        default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
-        filters_and_selections = default_sel_filt.split("&")
+    default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
+    filters_and_selections = default_sel_filt.split("&")
 
-        for fs in filters_and_selections:
-            if fs.startswith('$select='):
-                select = fs[8:]
+    for fs in filters_and_selections:
+        if fs.startswith('$filter='):
+            filt = fs[8:]
 
-        select_list = select.split(', ')
-        data = cbsodata.get_data('82070ENG', select=select_list[0:2])
+    cbsodata.get_data('82070ENG', filters=filt)
 
-        self.assertEqual(len(data[0].keys()), 2)
-        self.assertEqual(len(data[5].keys()), 2)
-        self.assertEqual(len(data[10].keys()), 2)
 
-    @parameterized.expand(datasets_derden)
-    def test_get_table_list_derden(self, table_id):
+def test_select():
 
-        # option 1
-        print("global")
-        cbsodata.options.catalog_url = 'dataderden.cbs.nl'
-        data_option1 = cbsodata.get_table_list()
-        cbsodata.options.catalog_url = 'opendata.cbs.nl'
+    default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
+    filters_and_selections = default_sel_filt.split("&")
 
-        # option 2
-        print("context")
-        with cbsodata.catalog('dataderden.cbs.nl'):
-            data_option2 = cbsodata.get_table_list()
+    for fs in filters_and_selections:
+        if fs.startswith('$select='):
+            select = fs[8:]
 
-        # option 3
-        print("argument")
-        data_option3 = cbsodata.get_table_list(
-            catalog_url='dataderden.cbs.nl'
-        )
+    cbsodata.get_data('82070ENG', select=select)
 
-        assert len(data_option1[0].keys()) > 0
 
-        for key in data_option1[0].keys():
+def test_select_list():
 
-            assert data_option1[0][key] == \
-                data_option2[0][key] == data_option3[0][key]
+    default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
+    filters_and_selections = default_sel_filt.split("&")
 
-    @parameterized.expand(datasets_derden)
-    def test_get_data_derden(self, table_id):
+    for fs in filters_and_selections:
+        if fs.startswith('$select='):
+            select = fs[8:]
 
-        # option 1
-        print("global")
-        cbsodata.options.catalog_url = 'dataderden.cbs.nl'
-        data_option1 = cbsodata.get_data(table_id)
-        cbsodata.options.catalog_url = 'opendata.cbs.nl'
+    cbsodata.get_data('82070ENG', select=select.split(', '))
 
-        # option 2
-        print("context")
-        with cbsodata.catalog('dataderden.cbs.nl'):
-            data_option2 = cbsodata.get_data(table_id)
 
-        # option 3
-        print("argument")
-        data_option3 = cbsodata.get_data(
-            table_id,
-            catalog_url='dataderden.cbs.nl'
-        )
+def test_select_subset():
 
-        assert len(data_option1[0].keys()) > 0
+    default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
+    filters_and_selections = default_sel_filt.split("&")
 
-        for key in data_option1[0].keys():
+    for fs in filters_and_selections:
+        if fs.startswith('$select='):
+            select = fs[8:]
 
-            assert data_option1[0][key] == \
-                data_option2[0][key] == data_option3[0][key]
+    select_list = select.split(', ')
+    cbsodata.get_data('82070ENG', select=select_list[0:2])
+
+
+def test_select_n_cols():
+
+    default_sel_filt = cbsodata.get_info('82070ENG')['DefaultSelection']
+    filters_and_selections = default_sel_filt.split("&")
+
+    for fs in filters_and_selections:
+        if fs.startswith('$select='):
+            select = fs[8:]
+
+    select_list = select.split(', ')
+    data = cbsodata.get_data('82070ENG', select=select_list[0:2])
+
+    assert len(data[0].keys()) == 2
+    assert len(data[5].keys()) == 2
+    assert len(data[10].keys()) == 2
+
+
+@pytest.mark.parametrize("table_id", datasets_derden)
+def test_get_table_list_derden(table_id):
+
+    # option 1
+    print("global")
+    cbsodata.options.catalog_url = 'dataderden.cbs.nl'
+    data_option1 = cbsodata.get_table_list()
+    cbsodata.options.catalog_url = 'opendata.cbs.nl'
+
+    # option 2
+    print("context")
+    with cbsodata.catalog('dataderden.cbs.nl'):
+        data_option2 = cbsodata.get_table_list()
+
+    # option 3
+    print("argument")
+    data_option3 = cbsodata.get_table_list(
+        catalog_url='dataderden.cbs.nl'
+    )
+
+    assert len(data_option1[0].keys()) > 0
+
+    for key in data_option1[0].keys():
+
+        assert data_option1[0][key] == \
+            data_option2[0][key] == data_option3[0][key]
+
+
+@pytest.mark.parametrize("table_id", datasets_derden)
+def test_get_data_derden(table_id):
+
+    # option 1
+    print("global")
+    cbsodata.options.catalog_url = 'dataderden.cbs.nl'
+    data_option1 = cbsodata.get_data(table_id)
+    cbsodata.options.catalog_url = 'opendata.cbs.nl'
+
+    # option 2
+    print("context")
+    with cbsodata.catalog('dataderden.cbs.nl'):
+        data_option2 = cbsodata.get_data(table_id)
+
+    # option 3
+    print("argument")
+    data_option3 = cbsodata.get_data(
+        table_id,
+        catalog_url='dataderden.cbs.nl'
+    )
+
+    assert len(data_option1[0].keys()) > 0
+
+    for key in data_option1[0].keys():
+
+        assert data_option1[0][key] == \
+            data_option2[0][key] == data_option3[0][key]
